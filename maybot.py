cea3e4f39d842a696f9e0085c647a5256bccfa44
Peter Carragher <petercarragher6@gmail.com>
2017-01-28 20:17:52 +0000

    substitutes placeholders in audio input

--- maybot.py
+++ maybot.py
@@ -21,16 +21,21 @@ from __future__ import print_function
 from flask import Flask, render_template
 from flask_ask import Ask, question, statement
 from pramesi_word_model.create_sentence import create_sentence
-
+from nltk.tag import pos_tag
+import re
 app = Flask(__name__)
 ask = Ask(app, '/')
 
-welcome_text  = "Hi! I complete sentences in the style of Theresa May. "\
-                "Use me by saying, 'Complete', followed by the first words of a sentence."
+welcome_text  = "Lets start a new story."\
+                "Once upon a time, in a land far far away "
 
-reprompt_text = "Use this skill by saying, 'Complete', followed by the first words of a sentence."
+reprompt_text = "What happens next?"
 
-goodbye_text  = "Goodbye from Maybot."
+goodbye_text  = "And they all lived happily ever after. The end."
+
+names = {}
+
+rand_names = set()
 
 @ask.launch
 def welcome():
@@ -38,6 +43,16 @@ def welcome():
 
 @ask.intent('CompleteSentenceIntent')
 def hello(InitialWords):
+
+    #update names with new names
+    tagged_sent = pos_tag(InitialWords.split())
+    [names.append(word) for word, pos in tagged_sent if pos == 'NNP' and word not in names]
+
+    #substitute place holders into initialwords
+    for name, pos in tagged_sent:
+        if pos == 'NNP':
+            InitialWords = re.sub(name, "person_"+str(names.index(name)), InitialWords)
+
     if InitialWords is None:
         InitialWords = ''
     speech_output = create_sentence(InitialWords, seed=0, diversity=0.0)
