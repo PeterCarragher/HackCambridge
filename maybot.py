60523e95e5fed65df1bc3d9ed26d6816f1c14c0f
Peter Carragher <petercarragher6@gmail.com>
2017-01-28 21:15:26 +0000

    using last command to fill up 12 words

--- maybot.py
+++ maybot.py
@@ -28,8 +28,7 @@ import random
 app = Flask(__name__)
 ask = Ask(app, '/')
 
-welcome_text  = "Lets start a new story."\
-                "Once upon a time, in a land far far away "
+welcome_text  = "Lets start a new story. Once upon a time, in a land far far away "
 
 reprompt_text = "What happens next?"
 
@@ -41,13 +40,15 @@ rand_names = ([name.lower() for name in names.words("male.txt")] + [name.lower()
 rand_names.append("andreea")
 random.shuffle(rand_names)
 
+lastOutput = welcome_text
+
 @ask.launch
 def welcome():
     return question(welcome_text).reprompt(reprompt_text).simple_card('Welcome', welcome_text)
 
 @ask.intent('CompleteSentenceIntent')
 def hello(InitialWords):
-
+    global lastOutput
     #update names with new names
     tagged_sent = pos_tag(InitialWords.split())
     [names.append(word) for word, pos in tagged_sent if pos == 'NNP' and word not in names]
@@ -60,7 +61,17 @@ def hello(InitialWords):
     if InitialWords is None:
         InitialWords = ''
 
+    #add 12-givenWords from lastOutput to InitialWords
+    if len(InitialWords.split())<12:
+        lastList = lastOutput.split()
+        numWordsNeeded = 12 - len(InitialWords.split())
+        listNeeded = lastList[len(lastList)-numWordsNeeded:]
+        space = " "
+        InitialWords += space.join(listNeeded)
+
     speech_output = create_sentence(InitialWords, seed=0, diversity=0.0)
+
+    lastOutput = speech_output
 
     #substitute names back into speech_output
     iters = [m.start() for m in re.finditer('person_', speech_output)]
@@ -73,6 +84,7 @@ def hello(InitialWords):
             name = rand_names[personNum-len(names)]
 
         speech_output = re.sub("person_"+str(personNum), name, speech_output)
+
 
     return question(speech_output).reprompt(reprompt_text).simple_card('CompleteSentenceIntent', speech_output)
 
